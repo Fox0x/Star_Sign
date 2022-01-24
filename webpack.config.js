329e00b8be875a763107a3c4398c3d86bff6a597
Kirill <foxxx2game@gmail.com>
2022-01-24 12:29:02 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development", // 'production'
+	entry: ["@babel/polyfill", "./src/index.jsx"], // entry point
+	output: {
+		filename: "[name].[hash].js", // output file
+		path: path.resolve(__dirname, "dist"), // output path
+	},
+	resolve: {
+		extensions: [".js", ".json", ".jsx", "..."],
+	},
+	devServer: {
+		port: 3000,
+	},
+	plugins: [ 
+		new HtmlWebpackPlugin({ 
+			template: "./src/index.html", 
+		}),
+		new CleanWebpackPlugin(), // clean dist folder
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(css)$/, // style loader
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/, // image loader
+				use: ["file-loader"],
+			},
+			{
+				test: /\.m?js$/, 
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.m?jsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+		],
+	},
+};
