7286823db2775bb7fb4ffc47e271511d1d97df9b
Kirill <foxxx2game@gmail.com>
2022-01-24 15:10:02 +0300

    add start page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
 import React from "react";
+import { StartPage } from "./StartPage/StartPage";
 
 export const App = () => {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<StartPage/>
 	);
 };
---
+++ src/components/StartPage/StartPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import css from './StartPage.module.css';
+import background from '../../../public/images/background.png'
+
+export const StartPage = () => {
+  return <div className={css.startPage__wrapper} style={{backgroundImage: `url(${background})`}}>
+hhhhhhhhh
+  </div>;
+};
